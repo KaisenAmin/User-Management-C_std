2e4318780165930f065d83451f8ec6cf1cd08ede
amin tahmasebi <amintahmasebi479@gmail.com>
2024-08-23 00:06:35 +0330

    add cmake to project with all dependency

---
+++ build/database/CMakeFiles/database.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../build/libdatabase.dll"
+  "../build/libdatabase.dll.manifest"
+  "../build/libdatabase.pdb"
+  "CMakeFiles/database.dir/postgres.c.obj"
+  "CMakeFiles/database.dir/postgres.c.obj.d"
+  "libdatabase.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
